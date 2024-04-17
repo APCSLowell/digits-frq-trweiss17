45e5c2eebc91c31ca01705e7b16f7004d9e52168
trweiss17 <142759304+trweiss17@users.noreply.github.com>
2024-04-16 21:01:34 -0700

    Update Digits.java

--- src/main/java/Digits.java
+++ src/main/java/Digits.java
@@ -8,7 +8,7 @@ public class Digits
 	public Digits(int num)
 	{ /* to be implemented in part (a) */ 
 		digitList= new ArrayList<Integer>();
-		if(num<0){
+		if(num<9){
 			digitList.add(num);
 		}
 		while(num>9){
